40c93eb266a58866b8ba7efb4c4fa55537be6de3
Kevin Bjørn <73933092+KevinDania@users.noreply.github.com>
2024-08-26 15:16:10 +0200

    added token with required before seeing the privat components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Dashboard from './components/Dashboard/Dashboard';
+import Login from './components/Login/Login';
 import Preferences from './components/Preferences/Preferences.js';
 
 function App() {
+  const [token, setToken] = useState();
+
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+
   return (
     <div className="wrapper">
       <h1>Application</h1>
