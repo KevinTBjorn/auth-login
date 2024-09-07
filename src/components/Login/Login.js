a24acede99fb1f00da7b8864ea530d057bca9fe9
Kevin Bjørn <73933092+KevinDania@users.noreply.github.com>
2024-09-07 16:17:56 +0200

    sumbit function

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -17,10 +17,19 @@ function Login( { setToken } ) {
   const [username, setUserName] = useState();
   const [password, setPassword] = useState();
 
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const token = await loginUser({
+      username,
+      password
+    });
+    setToken(token);
+  }
+
   return(
     <div className="login-wrapper">
       <h1>Please Log In</h1>
-      <form >
+      <form onSubmit={handleSubmit}>
         <label>
           <p>Username</p>
           <input type="text" onChange={e => setUserName(e.target.value)}/>
