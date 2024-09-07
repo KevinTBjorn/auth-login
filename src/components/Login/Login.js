d8f6066528fc8d17edba4626cb3efbd169a16af5
Kevin Bjørn <73933092+KevinDania@users.noreply.github.com>
2024-09-07 16:16:26 +0200

    added state and onchange

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
+import PropTypes from 'prop-types';
 
-function Login() {
+function Login( { setToken } ) {
+  const [username, setUserName] = useState();
+  const [password, setPassword] = useState();
+
   return(
     <div className="login-wrapper">
       <h1>Please Log In</h1>
-      <form>
+      <form >
         <label>
           <p>Username</p>
-          <input type="text" />
+          <input type="text" onChange={e => setUserName(e.target.value)}/>
         </label>
         <label>
           <p>Password</p>
-          <input type="password" />
+          <input type="password" onChange={e => setPassword(e.target.value)}/>
         </label>
         <div>
           <button type="submit">Submit</button>
@@ -20,6 +24,10 @@ function Login() {
       </form>
     </div>
   )
+}
+
+Login.propTypes = {
+  setToken: PropTypes.func.isRequired
 }
 
 export default Login;
