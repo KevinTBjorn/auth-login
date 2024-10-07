dc8235dd53bee682fdd96b717c6f5afd062929f5
Kevin Bjørn <73933092+KevinDania@users.noreply.github.com>
2024-10-07 15:59:00 +0200

    added sessionstorage

---
+++ src/components/App/App.js
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Dashboard from '../Dashboard/Dashboard.js';
+import Login from '../Login/Login.js';
+import Preferences from '../Preferences/Preferences.js';
+
+function setToken(userToken) {
+  sessionStorage.setItem('token', JSON.stringify(userToken));
+}
+
+function getToken() {
+  const tokenString = sessionStorage.getItem('token');
+  const userToken = JSON.parse(tokenString);
+  return userToken?.token
+}
+
+function App() {
+  //const [token, setToken] = useState();
+  const token = getToken();
+
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+
+  return (
+    <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />}/>
+          <Route path="/preferences" element={<Preferences />}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
--- src/components/Preferences/Preferences.js
+++ src/components/Preferences/Preferences.js
@@ -1,5 +1,3 @@
-import React from 'react';
-
 function Preferences() {
   return(
     <h2>Preferences</h2>
