b75eea79db02e4b0113dcff58b3e2e467394043b
Kevin Bjørn <73933092+KevinDania@users.noreply.github.com>
2024-10-16 17:28:39 +0200

    added get/save token as a hook

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,16 +5,6 @@ import Dashboard from '../Dashboard/Dashboard.js';
 import Login from '../Login/Login.js';
 import Preferences from '../Preferences/Preferences.js';
 
-function setToken(userToken) {
-  sessionStorage.setItem('token', JSON.stringify(userToken));
-}
-
-function getToken() {
-  const tokenString = sessionStorage.getItem('token');
-  const userToken = JSON.parse(tokenString);
-  return userToken?.token
-}
-
 function App() {
   //const [token, setToken] = useState();
   const token = getToken();
---
+++ src/components/App/UserToken.js
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+
+function useToken() {
+  const [token, setToken] = useState(getToken());
+
+  function getToken() {
+    const tokenString = sessionStorage.getItem('token');
+    const userToken = JSON.parse(tokenString);
+    return userToken?.token
+  }
+
+  function saveToken(userToken) {
+    sessionStorage.setItem('token', JSON.stringify(userToken));
+    setToken(userToken.token);
+  }
+
+  return {
+    setToken: saveToken,
+    token
+  }
+}
+
+export default useToken;
